fe370f7635cea7ed81f97a257738578fa97a2e15
alvilika <alvilika15@gmail.com>
2025-06-23 23:57:47 +0200

    minimal home page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {InfobarComponent} from './shared/components/infobar/infobar.component';
+import {HeaderComponent} from './shared/components/header/header.component';
+import {FooterComponent} from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InfobarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
---
+++ src/app/shared/components/infobar/infobar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infobar',
+  imports: [],
+  standalone:true,
+  templateUrl: './infobar.component.html',
+  styleUrl: './infobar.component.css'
+})
+export class InfobarComponent {
+
+}
